46a228ec1366c66556f46664f81ad3fdad1e3420
PeaNu <peanu.xue@kooppi.com>
2023-08-06 14:07:23 +0800

    add deploy stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/perfect-world-npc-search-system/',
   plugins: [react()],
   server: {
     host: true
